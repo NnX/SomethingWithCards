53cddcd68525035f9c952a826b91952d32dfe288
NnX <hentis@ukr.net>
2024-08-03 17:36:48 +0300

    set target frame rate to 30

---
+++ Assets/Scripts/Utils/TargetFrameRate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Utils
+{
+    public class TargetFrameRate : MonoBehaviour
+    {
+        private void Start()
+        {
+            Application.targetFrameRate = 30; 
+        }
+    }
+}
